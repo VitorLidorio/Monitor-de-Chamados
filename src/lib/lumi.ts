19c4fc5abad1802d5aad6d6aec0665a7af1fd96a
VitorLidorio <133905864+VitorLidorio@users.noreply.github.com>
2026-02-05 16:34:55 -0300

    Delete src directory

--- src/lib/lumi.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-  projectId: 'p384419209373888512',
-  apiBaseUrl: 'https://api.lumi.new',
-  authOrigin: 'https://auth.lumi.new',
-})
